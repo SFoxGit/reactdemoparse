effcd180a934c5ee6a8c04c60b3a8f3acc05de88
Shawn Fox <foxeasttrojan06@gmail.com>
2021-08-03 17:48:57 -0400

    summary stats test

--- src/Add/add.jsx
+++ src/Add/add.jsx
@@ -7,62 +7,7 @@ export default function Add(props) {
   const myForm = useRef();
   const csvFile = useRef();
 
-  function csvToArray(str, delimiter = ",") {
-
-    // slice from start of text to the first \n index
-    // use split to create an array from string by delimiter
-    const headers = str.slice(0, str.indexOf("\n")).split(delimiter);
-
-    // slice from \n index + 1 to the end of the text
-    // use split to create an array of each csv value row
-    const rows = str.slice(str.indexOf("\n") + 1).split("\n");
-
-    // Map the rows
-    // split values from each row into an array
-    // use headers.reduce to create an object
-    // object properties derived from headers:values
-    // the object passed as an element of the array
-    const arr = rows.map(function (row) {
-      const values = row.split(delimiter);
-      const el = headers.reduce(function (object, header, index) {
-        object[header] = values[index];
-        return object;
-      }, {});
-      return el;
-    });
-
-    // return the array
-    return arr;
-  }
-
-  const submit = (e) => {
-    e.preventDefault();
-    // const input = csvFile.files[0];
-    const reader = new FileReader();
-
-    reader.onload = function (e) {
-      const text = csvFile.current;
-      const data = csvToArray(text);
-      console.log(JSON.stringify(data));
-      setMatchData(JSON.stringify(data));
-    };
-
-    // reader.readAsText(input);
-  };
-
-  const handleOnDrop = (data) => {
-    console.log(data)
-  }
-
-  const handleOnError = (err, file, inputElem, reason) => {
-    console.log(err);
-  };
-
-  const handleOnRemoveFile = (data) => {
-    console.log('---------------------------');
-    console.log(data);
-    console.log('---------------------------');
-  };
+ 
 
   return (
     <Container>
--- src/Results/results.jsx
+++ src/Results/results.jsx
@@ -11,8 +11,24 @@ export default function Results(props) {
   //   setMatchData(json)
   // }, [])
   return (
-    <Container>
-      {matchData.map(data => <Row>{data.map(el => <Col>{el}</Col>)}</Row>)}
+    <Container fluid>
+      {/* {matchData.map(data => <Row>{data.map(el => <Col>{el}</Col>)}</Row>)} */}
+      {matchData.map(data => <Row>{data[2] === "summary_stats" ?
+        <>
+          <Col xs={2}>{data[3]}</Col>
+          <Col xs={1}>{data[4]}</Col>
+          <Col xs={1}>{data[8]}</Col>
+          <Col xs={1}>{data[14]}</Col>
+          <Col xs={1}>{data[15]}</Col>
+          <Col xs={1}>{(data[16] * 100).toFixed(2) + '%'}</Col>
+          <Col xs={1}>{(data[17] * 100).toFixed(2) + '%'}</Col>
+          <Col xs={1}>{(data[18] * 100).toFixed(2) + '%'}</Col>
+          <Col xs={1}>{data[19]}</Col>
+        </>
+        :
+        null
+      }
+      </Row>)}
     </Container>
   )
 }
