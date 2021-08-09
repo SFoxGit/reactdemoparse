467fa0bfc31f81a9ff8bed7343dea35caa0a6e06
Shawn Fox <foxeasttrojan06@gmail.com>
2021-08-08 21:23:41 -0400

    router-dom links

--- client/src/Header/header.jsx
+++ client/src/Header/header.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import { Navbar, Nav, Container } from 'react-bootstrap'
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom'
 
 function Header() {
 
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand><Link to="/">CoH PvP DemoParse</Link></Navbar.Brand>
+        <Navbar.Brand as={Link} to="/">CoH PvP DemoParse</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ml-auto">
-            <Nav><Link to="/results">Results</Link></Nav>
-            <Nav><Link to="/add">Add</Link></Nav>
-            <Nav><Link to="/login">Login</Link></Nav>
+            <Nav.Link as={Link} to="/results">Results</Nav.Link>
+            <Nav.Link as={Link} to="/add">Add</Nav.Link>
+            <Nav.Link as={Link} to="/login">Login</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
