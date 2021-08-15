a3a67da3014cb89095e126df65cc6c702952fa2b
Shawn Fox <foxeasttrojan06@gmail.com>
2021-08-14 21:48:43 -0400

    header check logged in for collection

--- client/src/Header/header.jsx
+++ client/src/Header/header.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import { Navbar, Nav, Container } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
-function Header() {
-
+function Header(props) {
+const loggedIn = props.loggedIn
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
@@ -13,7 +13,7 @@ function Header() {
           <Nav className="ml-auto">
             {/* <Nav.Link as={Link} to="/results">Results</Nav.Link> */}
             {/* <Nav.Link as={Link} to="/add">Add</Nav.Link> */}
-            <Nav.Link as={Link} to="/collections">Collections</Nav.Link>
+            {loggedIn ? <Nav.Link as={Link} to="/collections">Collections</Nav.Link> : null }
             <Nav.Link as={Link} to="/login">Login</Nav.Link>
           </Nav>
         </Navbar.Collapse>
